f78c2241a11336c29aff6fdafd26f7ceac6d026a
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-05-29 20:43:24 +0530

    home logout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {  Button } from 'antd';
+import { getSessionData,removeSessionData } from './utils/common'
 
 export default function Home({history}) {
+
+    
+    const logout = () => {
+        removeSessionData()
+        history.push('/login')
+    }
     return (
         <div>
             <h1>Home</h1>
-            <Button><Link to='/login' >login</Link></Button>
-            <Button onClick={() => {history.push('/login')}} >logout</Button>
+            {
+                getSessionData() ? <Button onClick={logout} >logout</Button> :  <Button><Link to='/login' >login</Link></Button>
+            }
         </div>
     )
 }
