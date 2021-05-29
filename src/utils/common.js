94b392a22cd83bac6426cf22caead52e19028b65
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-05-29 20:39:17 +0530

    authhesder

--- src/utils/common.js
+++ src/utils/common.js
@@ -13,3 +13,14 @@ export const setSessionData = (user) => {
 export const removeSessionData = () => {
     localStorage.removeItem('user')
 }
+
+export function authHeader() {
+    // return authorization header with jwt token
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.token) {
+        return { 'Authorization': 'Bearer ' + user.token };
+    } else {
+        return {};
+    }
+}
