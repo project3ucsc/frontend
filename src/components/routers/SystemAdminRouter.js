934b830d5a1206cc24f9162baa14647bc50491cf
shamiSathindra <shamisathindra@gmail.com>
2021-07-15 18:37:59 +0530

    System admin_Education prohramme

--- src/components/routers/SystemAdminRouter.js
+++ src/components/routers/SystemAdminRouter.js
@@ -1,13 +1,16 @@
 import { Route, Switch } from "react-router-dom";
-
-import Dashboard from "pages/teacher/Dashboard";
+import Dashboard from "pages/sysadmin/Dashboard";
+import EduProg from "pages/sysadmin/EduProg";
+import Home from "pages/sysadmin/Home";
 import NotFound404 from "pages/NotFound404";
 
 export default function SystemAdminRouter() {
   return (
     <>
       <Switch>
+        <Route exact path="/home" component={Home} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/eduProg" component={EduProg} />
 
         <Route component={NotFound404} />
       </Switch>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col, Calendar } from "antd";
+import img1 from "../img/sysadmin1.png";
 import ContentLayout from "components/ContentLayout";
+import "./sysadmin/sysadmin.scss";
+import Paragraph from "antd/lib/skeleton/Paragraph";
 
 export default function Home() {
-  const { Content } = Layout;
+  function onPanelChange(value, mode) {
+    console.log(value, mode);
+    console.log(img1);
+  }
 
+  const { Content } = Layout;
   return (
     <ContentLayout paths={["Home"]}>
       <Content
@@ -14,8 +21,25 @@ export default function Home() {
           margin: 0,
           minHeight: 280,
         }}
+
+      
       >
-        Bla bla home
+        <Row>
+          <Col xs={24} xl={18}>
+            <div className= "sysadmin-header">
+              <h1>Welcome Admin <img src={img1} alt="img1" /></h1> 
+             
+          
+          </div>
+          </Col>
+
+          <Col xs={24} xl={6}>
+            <div className="site-calendar-demo-card">
+              <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+            </div>
+            ,
+          </Col>
+        </Row>
       </Content>
     </ContentLayout>
   );
