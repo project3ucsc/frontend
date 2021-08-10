34d77acb8f4f99e4eb768cd036fe7a3600d0119f
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-08-10 10:43:11 +0530

    student timetable data dynamycally loding

--- src/pages/student/StudentTimeTable.jsx
+++ src/pages/student/StudentTimeTable.jsx
@@ -1,53 +1,32 @@
-import React from "react";
-import { Layout, Row, Col, Tabs } from "antd";
+import React, { useEffect, useState } from "react";
+import { Layout, Row, Col, Tabs, message, Spin } from "antd";
 import ContentLayout from "components/ContentLayout";
 import TimeTableBig from "components/TimeTableBig";
 import TimeTableSm from "components/TimeTableSm";
+import timeslotservice from "services/timeslot.service";
 
 const { TabPane } = Tabs;
 
-const subjects = ["Maths", "Chemistry", "Physics", "English"];
-const data = [
-  {
-    key: 1,
-    time: "08.00-09.10",
-    mon: subjects[0],
-    tue: subjects[0],
-    wed: subjects[0],
-    thu: subjects[0],
-    fri: subjects[0],
-  },
-  {
-    key: 2,
-    time: "09.10-10.30",
-    mon: subjects[1],
-    tue: subjects[1],
-    wed: subjects[3],
-    thu: subjects[2],
-    fri: subjects[3],
-  },
-  {
-    key: 3,
-    time: "11.10-12.10",
-    mon: subjects[2],
-    tue: subjects[3],
-    wed: subjects[1],
-    thu: subjects[1],
-    fri: subjects[2],
-  },
-  {
-    key: 4,
-    time: "12.10-13.30",
-    mon: subjects[3],
-    tue: subjects[2],
-    wed: subjects[2],
-    thu: subjects[3],
-    fri: subjects[1],
-  },
-];
-
 const { Content } = Layout;
 export default function StudentTimeTable() {
+  const [loading, setLoading] = useState(true);
+  const [tsdata, setTsdata] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    timeslotservice
+      .getTimeSlotsForStudent()
+      .then((data) => {
+        setTsdata(data);
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((e) => {
+        setLoading(false);
+        message.error(e.message);
+      });
+  }, []);
+
   return (
     <ContentLayout title="Timetable" paths={["Home", "Timetable"]}>
       <Content
@@ -60,27 +39,29 @@ export default function StudentTimeTable() {
       >
         <Row>
           <Col xs={24} xl={24}>
-            {window.innerWidth > 768 ? (
-              <TimeTableBig data={data} />
-            ) : (
-              <Tabs defaultActiveKey="1" centered>
-                <TabPane tab="Mon" key="1">
-                  <TimeTableSm data={data} day="mon" />
-                </TabPane>
-                <TabPane tab="Tue" key="2">
-                  <TimeTableSm data={data} day="tue" />
-                </TabPane>
-                <TabPane tab="Wed" key="3">
-                  <TimeTableSm data={data} day="wed" />
-                </TabPane>
-                <TabPane tab="Thu" key="4">
-                  <TimeTableSm data={data} day="thu" />
-                </TabPane>
-                <TabPane tab="Fri" key="5">
-                  <TimeTableSm data={data} day="fri" />
-                </TabPane>
-              </Tabs>
-            )}
+            {!loading &&
+              (window.innerWidth > 768 ? (
+                <TimeTableBig data={tsdata} />
+              ) : (
+                <Tabs defaultActiveKey="1" centered>
+                  <TabPane tab="Mon" key="1">
+                    <TimeTableSm data={tsdata} day={1} />
+                  </TabPane>
+                  <TabPane tab="Tue" key="2">
+                    <TimeTableSm data={tsdata} day={2} />
+                  </TabPane>
+                  <TabPane tab="Wed" key="3">
+                    <TimeTableSm data={tsdata} day={3} />
+                  </TabPane>
+                  <TabPane tab="Thu" key="4">
+                    <TimeTableSm data={tsdata} day={4} />
+                  </TabPane>
+                  <TabPane tab="Fri" key="5">
+                    <TimeTableSm data={tsdata} day={5} />
+                  </TabPane>
+                </Tabs>
+              ))}
+            {loading && <Spin size="large" />}
           </Col>
         </Row>
       </Content>
