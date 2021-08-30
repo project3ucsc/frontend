f547e02fc50677103956f0bdbd4ae5e7efab43e4
Harishchandra98 <harishchandrads98@gmail.com>
2021-08-30 11:32:50 +0530

    update manage user accounts

--- src/pages/scladmin/ManageUsers.jsx
+++ src/pages/scladmin/ManageUsers.jsx
@@ -7,9 +7,12 @@ import {
     Input,
     Space,
     Modal,
+    Button,
+    Descriptions,
 } from "antd";
 
 import ContentLayout from "components/ContentLayout";
+import "./ManageUsers.scss";
 
 const { Content } = Layout;
 const { Search} = Input;
@@ -22,14 +25,45 @@ export default function ManageUsers() {
     return (
         <ContentLayout title="Manage Users" paths={["SchoolAdmin", "ManageUsers"]}>
             <Modal 
-
+              visible={studentvisible}
+              title="Manage Account"
+              onCancel={ () => setstudentvisible(false)}
+              footer={[
+                  <Button onClick={ () => setstudentvisible(false)}>Cancel</Button>,
+                  <Button danger onClick={ () => setstudentvisible(false)}>Deactivate</Button>,
+                  <Button type="primary" onClick={ () => setstudentvisible(false)}>OK</Button>,
+              ]}
             >
-
+                <Descriptions>
+                    <Descriptions.Item label="id">1001</Descriptions.Item>
+                    <Descriptions.Item label="Name" span={2}>Vishmi Herath</Descriptions.Item>
+                    <Descriptions.Item label="Email">vishmi.he@gamil.com</Descriptions.Item>
+                </Descriptions>
+            </Modal>
+            <Modal 
+              visible={teachervisible}
+              title="Manage Account"
+              onCancel={ () => setteachervisible(false)}
+              footer={[
+                  <Button onClick={ () => setteachervisible(false)}>Cancel</Button>,
+                  <Button danger onClick={ () => setteachervisible(false)}>Deactivate</Button>,
+                  <Button type="primary" onClick={ () => setteachervisible(false)}>Activate</Button>,
+              ]}
+            >
+                <Descriptions>
+                    <Descriptions.Item label="id">T001</Descriptions.Item>
+                    <Descriptions.Item label="Name" span={2}>Upamali Karunanayake</Descriptions.Item>
+                    <Descriptions.Item label="Email">upamalik@gmail.com</Descriptions.Item>
+                </Descriptions>
             </Modal>
            <Content>
-              <Row gutter={16}>
-                  <Col xs={24} xl={12}>
-                      <Card title="Manage Student Accounts" bordered={true}>
+              <Row gutter={[16,32]}>
+                  <Col lg={6}></Col>
+                  <Col lg={12}>
+                      <Card className="scladmincard"
+                        title="Manage Student Accounts" 
+                        bordered={true}
+                      >
                           <Space direction="vertical">
                             <Search 
                                 placeholder="student id"
@@ -41,7 +75,10 @@ export default function ManageUsers() {
                         
                       </Card>
                   </Col>
-                  <Col xs={24} xl={12}>
+                  <Col lg={6}></Col>
+
+                  <Col lg={6}></Col>
+                  <Col lg={12}>
                       <Card title="Manage Teacher Accounts" bordered={true}>
                         <Space direction="vertical">
                             <Search 
@@ -53,6 +90,7 @@ export default function ManageUsers() {
                           </Space>
                       </Card>
                   </Col>
+                  <Col lg={6}></Col>
               </Row>
            </Content>
         </ContentLayout>
