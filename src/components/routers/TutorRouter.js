040bade79d6a6d143500ab0c6327efe068d5ef78
Shashi <shashinitharuka9@gmail.com>
2021-09-16 19:43:32 +0530

    Tutor Dashboard Added #1

--- src/components/routers/TutorRouter.js
+++ src/components/routers/TutorRouter.js
@@ -4,6 +4,7 @@ import Home from "pages/Home";
 import PaymentSlipCheck from "pages/tutor/PaymentSlipCheck";
 import Dashboard from "pages/tutor/Dashboard";
 import ApplicationForm from "pages/tutor/ApplicationForm";
+import ClassDetails from "pages/tutor/ClassDetails";
 import NotFound404 from "pages/NotFound404";
 
 export default function TutorRouter() {
@@ -14,6 +15,7 @@ export default function TutorRouter() {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/paymentslipcheck" component={PaymentSlipCheck} />
             <Route exact path="/applicationform" component={ApplicationForm} />
+            <Route exact path="/classdetails" component={ClassDetails} />
             <Route component={NotFound404} />
         </Switch>
         </>
--- src/pages/tutor/Dashboard.jsx
+++ src/pages/tutor/Dashboard.jsx
@@ -1,13 +1,68 @@
 import React from "react";
-
+import { Layout, Row, Col, Card } from "antd";
 import ContentLayout from "components/ContentLayout";
+import { Link } from "react-router-dom"; 
 
-export default function Dashboard(){
-    return (
-        <ContentLayout
-          title="Dashboard"
-          paths={["Tutor","Dashboard"]}
-        >
-        </ContentLayout>
-    );
+import { Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+
+export default function Dashboard() {
+  const classes = ["Maths", "Chemistry", "Physics"];
+  const { Content } = Layout;
+
+  const { Meta } = Card;
+  
+  return (
+    <ContentLayout title="Dashboard" paths={["Home", "Dashboard"]}>
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          margin: 0,
+          minHeight: 280,
+        }}
+      >
+        <Row>
+          <Col xs={24} xl={18}>
+            <div className="card-wrapper">
+              {classes.map((classt, index) => {
+                return (
+
+                  <Link to="/classdetails">
+                    <Card
+                      style={{ width: 300 }}
+                      cover={
+                        <img
+                          alt="example"
+                          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                        />
+                      }
+                      actions={[
+                        <SettingOutlined key="setting" />,
+                        <EditOutlined key="edit" />,
+                        <EllipsisOutlined key="ellipsis" />,
+                      ]}
+                    >
+                      <Meta
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                        title={classt}
+                        description="Grade Day Time"
+                      />
+                      
+                    </Card>
+                  </Link>
+                
+
+                );
+              })}
+            </div>
+          </Col>
+          {/*<Col xs={24} xl={6}>
+            sdgsdg
+            </Col>*/}
+        </Row>
+      </Content>
+    </ContentLayout>
+  );
 }
