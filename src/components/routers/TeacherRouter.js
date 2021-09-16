f384b639632908685074ce74e9f30e828b69af89
Shashi <shashinitharuka9@gmail.com>
2021-09-16 13:09:33 +0530

    User Profile Components Added to all the users

--- src/components/routers/TeacherRouter.js
+++ src/components/routers/TeacherRouter.js
@@ -8,6 +8,7 @@ import ClassInCharge from "pages/teacher/ClassInCharge";
 import VideoPage from "pages/VideoPage";
 import SubmissionsPage from "pages/teacher/SubmissionsPage";
 import AssessmentPage from "pages/teacher/AssessmentPage";
+import Profile from "pages/student/Profile";
 
 // import PrivateRoute from "utils/PrivateRoute";
 // import { Role } from "utils/common";
@@ -31,7 +32,7 @@ export default function TeacherRouter() {
         />
         <Route exact path="/applyleave" component={ApplyLeave} />
         <Route exact path="/classincharge" component={ClassInCharge} />
-
+        <Route exact path="/profile" component={Profile} />
         <Route component={NotFound404} />
       </Switch>
     </>
