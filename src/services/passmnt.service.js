70f6f37340bc40798922e3c3e4b38f06becbd559
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-09-18 19:55:15 +0530

    tutor assesmnts

---
+++ src/services/passmnt.service.js
@@ -0,0 +1,106 @@
+// import React from 'react'
+import axios from "axios";
+import { authHeader } from "utils/authheader";
+import { apiurl } from "utils/common";
+import authenticationservice from "./authentication.service";
+
+async function getAssmnts(sdid) {
+  try {
+    const res = await axios.get(`${apiurl}/passmnt/all/${sdid}`, authHeader());
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+async function getAssmntByID(assid) {
+  try {
+    const res = await axios.get(`${apiurl}/passmnt/${assid}`, authHeader());
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function getAssmntByIdWithSubmisstion(assid) {
+  try {
+    const stuid = authenticationservice.currentUserValue.id;
+
+    const res = await axios.get(
+      `${apiurl}/passmnt/attach/${assid}/${stuid}`,
+      authHeader()
+    );
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function addAssmnt(data) {
+  try {
+    const res = await axios.post(`${apiurl}/passmnt`, data, authHeader());
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function updateAssmnt(assid, property, value) {
+  try {
+    const res = await axios.patch(
+      `${apiurl}/passmnt`,
+      { assid, property, value },
+      authHeader()
+    );
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function deleteAssmnt(psid) {
+  try {
+    const res = await axios.delete(`${apiurl}/passmnt/${psid}`, authHeader());
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function getSubmissions(assid) {
+  try {
+    const res = await axios.get(
+      `${apiurl}/passmnt/submission/all/${assid}`,
+      authHeader()
+    );
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+async function upsertSubmission(dbid, filename) {
+  try {
+    const stuid = authenticationservice.currentUserValue.id;
+    const assid = parseInt(dbid.split(".")[0]);
+    const subid = parseInt(dbid.split(".")[1]);
+    const res = await axios.put(
+      `${apiurl}/passmnt/submission`,
+      { subid, assid, stuid, filename },
+      authHeader()
+    );
+    return res.data;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
+}
+
+const assmntservice = {
+  getAssmnts,
+  getAssmntByID,
+  getAssmntByIdWithSubmisstion,
+  addAssmnt,
+  updateAssmnt,
+  getSubmissions,
+  upsertSubmission,
+};
+export default assmntservice;
