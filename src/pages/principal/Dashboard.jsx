6cb8d7ae32ea19761400084940843f8cde271f98
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-09-22 01:22:31 +0530

    dashboard redesign

--- src/pages/principal/Dashboard.jsx
+++ src/pages/principal/Dashboard.jsx
@@ -6,10 +6,11 @@ import {
   Card,
   Button,
   Statistic,
-  Tree,
-  Alert,
+  Avatar,
+  Tag,
   Space,
   Progress,
+  List,
   Divider,
   Modal,
   Form,
@@ -18,10 +19,65 @@ import {
   Select,
   Tabs,
 } from "antd";
+import { Line } from "react-chartjs-2";
 import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 import img1 from "../../img/admin_cover2.jpg";
 import ContentLayout from "components/ContentLayout";
 import "./principal.scss";
+
+const data = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "AL",
+      data: [3, 5, 3, 6, 8, 12],
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+    },
+    {
+      label: "OL (Grade 10-11)",
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: "rgb(0, 158, 13)",
+      borderColor: "rgba(0, 158, 13, 0.2)",
+    },
+    {
+      label: "Grade 6-9",
+      data: [13, 9, 13, 3, 1, 6],
+      fill: false,
+      backgroundColor: "rgb(28, 40, 204)",
+      borderColor: "rgba(28, 40, 204, 0.2)",
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+const datalist = [
+  {
+    title: "Ant Design Title 1",
+  },
+  {
+    title: "Ant Design Title 2",
+  },
+  {
+    title: "Ant Design Title 3",
+  },
+  {
+    title: "Ant Design Title 4",
+  },
+];
 
 //import { FALSE } from 'node-sass';
 
@@ -106,30 +162,31 @@ export default function Dashboard() {
     },
   ];
 
-  const onSelect = (selectedKeys: React.Key[], info: any) => {
-    console.log("selected", selectedKeys, info);
-  };
+  // const onSelect = (selectedKeys: React.Key[], info: any) => {
+  //   console.log("selected", selectedKeys, info);
+  // };
 
-  const onCheck = (checkedKeys: React.Key[], info: any) => {
-    console.log("onCheck", checkedKeys, info);
-  };
+  // const onCheck = (checkedKeys: React.Key[], info: any) => {
+  //   console.log("onCheck", checkedKeys, info);
+  // };
+  const x = 7;
 
   return (
-    <ContentLayout paths={["Home"]}>
+    <ContentLayout title="Dashboard" paths={["Home"]}>
       <Content className="-header">
-        <img className="cover" src={img1} alt="img1" />
+        {/* <img className="cover" src={img1} alt="img1" /> */}
 
         <Row>
           <Col xs={24} xl={24}>
             <Row gutter={16}>
-              <Col span={6}>
+              <Col span={x}>
                 <Statistic
                   className="stat"
                   title="Total No of Students in the school"
                   value={855}
                 />
               </Col>
-              <Col span={6}>
+              <Col span={x}>
                 <Statistic
                   className="stat"
                   title="Total No of Teachers in the school"
@@ -144,7 +201,12 @@ export default function Dashboard() {
                 />
               </Col>
 
-              <Button className="btn" type="primary" onClick={showModal}>
+              <Button
+                style={{ marginTop: 10 }}
+                className="btn"
+                type="primary"
+                onClick={showModal}
+              >
                 Edit school details
               </Button>
               <Modal
@@ -188,168 +250,242 @@ export default function Dashboard() {
                 </Form>
               </Modal>
             </Row>
-            ,
-            <div className="site-statistic-demo-card">
-              <Row gutter={16}>
-                <Col span={6}>
-                  <Card>
+            <br />
+            <Row gutter={16}>
+              <Col span={19}>
+                <Row gutter={16}>
+                  <Col span={16}>
+                    {" "}
+                    <Card
+                      className="dashcard"
+                      title="Teaching performance with sections"
+                    >
+                      <Line height={200} data={data} options={options} />
+                    </Card>
+                  </Col>
+                  <Col span={8}>
+                    <Card
+                      className="dashcard"
+                      title="Top 5 Best performing teachers"
+                      style={{ marginBottom: 16 }}
+                    >
+                      <List
+                        itemLayout="horizontal"
+                        dataSource={datalist}
+                        renderItem={(item) => (
+                          <List.Item>
+                            <List.Item.Meta
+                              avatar={
+                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                              }
+                              title={item.title}
+                              description={
+                                <p>
+                                  {" "}
+                                  Perfomance pecentage{" "}
+                                  <Tag color="blue">80%</Tag>
+                                </p>
+                              }
+                            />
+                          </List.Item>
+                        )}
+                      />
+                    </Card>
+                    <Card
+                      className="dashcard"
+                      title="Lowest perforrming teachers"
+                    >
+                      <List
+                        itemLayout="horizontal"
+                        dataSource={datalist}
+                        renderItem={(item) => (
+                          <List.Item>
+                            <List.Item.Meta
+                              avatar={
+                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                              }
+                              title={item.title}
+                              description={
+                                <p>
+                                  {" "}
+                                  Perfomance pecentage{" "}
+                                  <Tag color="blue">20%</Tag>
+                                </p>
+                              }
+                            />
+                          </List.Item>
+                        )}
+                      />
+                    </Card>
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col span={5}>
+                <Card className="dashcard" title="User enrollment Progress">
+                  <Card
+                    style={{ border: "#34393d5c 1px solid", marginBottom: 10 }}
+                  >
                     <p className="analysis">
                       {" "}
                       Students Enrolled in the System{" "}
                     </p>
-                    <Progress ClassName="chart"
+                    <Progress
+                      ClassName="chart"
                       type="circle"
                       percent={75}
                       format={(percent) => `${percent}% `}
                     />
                   </Card>
-                </Col>
 
-                <Col span={6}>
-                  <Card>
+                  <Card
+                    style={{ border: "#34393d5c 1px solid", marginBottom: 10 }}
+                  >
                     <p className="analysis">
                       {" "}
                       Teachers Enrolled in the System{" "}
                     </p>
-                    <Progress ClassName="chart"
+                    <Progress
+                      ClassName="chart"
                       type="circle"
                       percent={40}
                       format={(percent) => `${percent}% `}
                     />
                   </Card>
-                </Col>
 
-                <Col span={6}>
-                  <Card>
+                  <Card
+                    style={{ border: "#34393d5c 1px solid", marginBottom: 10 }}
+                  >
                     <p className="analysis"> System Access Rate of School </p>
-                    <Progress ClassName="chart"
+                    <Progress
+                      ClassName="chart"
                       type="circle"
                       colour="Green"
                       percent={100}
                       format={(percent) => `${percent}  `}
                     />
                   </Card>
-                </Col>
-                <Col span={6}>
-                  <Card>
-                    <h4 className="T_overview"> Overview of the school</h4>
-                    <Tree
-                      checkable
-                      defaultExpandedKeys={["0-0-0", "0-0-1"]}
-                      defaultSelectedKeys={["0-0-0", "0-0-1"]}
-                      defaultCheckedKeys={["0-0-0", "0-0-1"]}
-                      onSelect={onSelect}
-                      onCheck={onCheck}
-                      treeData={treeData}
-                    />
-                  </Card>
-                </Col>
-              </Row>
-            </div>
-            ,
-          </Col>
-        </Row>
-        <Row>
-          <Col span={18}>
-            <Card className="card-1">
-              <Form className="filter table">
-                <Form.Item
-                  span
-                  className="form"
-                  name="month-picker"
-                  label="Select Month"
-                >
-                  <DatePicker picker="month" />
-                </Form.Item>
-
-                <Form.Item
-                  span
-                  className="form"
-                  name="subject"
-                  label="Select Grade"
-                >
-                  <Select>
-                    <Select.Option value="1">Grade 1</Select.Option>
-                    <Select.Option value="1">Grade 2</Select.Option>
-                  </Select>
-                </Form.Item>
-                <Form.Item
-                  span
-                  className="form"
-                  name="grade"
-                  label="Select Subject"
-                >
-                  <Select>
-                    <Select.Option value="Maths">Maths</Select.Option>
-                    <Select.Option value="Science">Science</Select.Option>
-                  </Select>
-                </Form.Item>
-                <Form.Item
-                  name="teacher"
-                  className="form"
-                  label="Select Teacher"
-                  column-width="40px"
-                >
-                  <Select>
-                    <Select.Option value="1">Mr. Aruna Peris</Select.Option>
-                    <Select.Option value="2">Ms.Amala Rajakumara</Select.Option>
-                  </Select>
-                </Form.Item>
-                <Form.Item>
-                  <Button className="btn" type="primary" htmlType="submit">
-                    Create Report
-                  </Button>
-                </Form.Item>
-              </Form>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={18}>
-            <Card className="reports">
-              <p className="T-report"> Analysis Report</p>
-              <Tabs defaultActiveKey="1" centered>
-                <TabPane tab="Attendance Analysis of the Teacher" key="1">
-                  <Card Class name="details">
-                    <p>No of working days for the Month: </p>
-                    <p>No of periods for the Month : </p>
-                    <p>No of students assign for the teacher : </p>
-                    <p>No of leaves applied for the Month : </p>
-                    <h3>Attendance Percentage of ..(entered techer name)...  for the Month of ...(Entered month)... </h3>
-                    <Progress ClassName="chart"
-                      type="circle"
-                      percent={75} 
-                      format={(percent) => `${percent}% `}
-                    />
-
-                  </Card>
-                </TabPane>
-                <TabPane tab="Attendance Analysis of Students" key="2">
-                  <p>No of Students enrolled in particular subject : </p>
-                  <p>Required attendance for the month : </p>
-                  <p>Marked attendance for the month : </p>
-                  <h3> Attendance Analysis for the ..Subject.. </h3>
-                  <Progress ClassName="chart"
-                      type="circle"
-                      percent={75} 
-                      format={(percent) => `${percent}% `}
-                    />
-                </TabPane>
-                <TabPane tab="Subject Preference Status" key="3">
-                  <p>Total No of Students enrolled for ..Subject.. : </p>
-                  <p>No of students enrolled with teacher .. name.. : </p>
-                  <h3>Subject preference status</h3>
-                  <Progress ClassName="chart"
-                      type="circle"
-                      percent={75} 
-                      format={(percent) => `${percent}% `}
-                    />
-                </TabPane>
-              </Tabs>
-            </Card>
+                </Card>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Content>
     </ContentLayout>
   );
 }
+
+/* <Col span={6}>
+  <Card>
+    <h4 className="T_overview"> Overview of the school</h4>
+    <Tree
+      checkable
+      defaultExpandedKeys={["0-0-0", "0-0-1"]}
+      defaultSelectedKeys={["0-0-0", "0-0-1"]}
+      defaultCheckedKeys={["0-0-0", "0-0-1"]}
+      onSelect={onSelect}
+      onCheck={onCheck}
+      treeData={treeData}
+    />
+  </Card>
+</Col> */
+
+// <Card className="card-1">
+// <Form className="filter table">
+//   <Form.Item
+//     span
+//     className="form"
+//     name="month-picker"
+//     label="Select Month"
+//   >
+//     <DatePicker picker="month" />
+//   </Form.Item>
+
+//   <Form.Item
+//     span
+//     className="form"
+//     name="subject"
+//     label="Select Grade"
+//   >
+//     <Select>
+//       <Select.Option value="1">Grade 1</Select.Option>
+//       <Select.Option value="1">Grade 2</Select.Option>
+//     </Select>
+//   </Form.Item>
+//   <Form.Item
+//     span
+//     className="form"
+//     name="grade"
+//     label="Select Subject"
+//   >
+//     <Select>
+//       <Select.Option value="Maths">Maths</Select.Option>
+//       <Select.Option value="Science">Science</Select.Option>
+//     </Select>
+//   </Form.Item>
+//   <Form.Item
+//     name="teacher"
+//     className="form"
+//     label="Select Teacher"
+//     column-width="40px"
+//   >
+//     <Select>
+//       <Select.Option value="1">Mr. Aruna Peris</Select.Option>
+//       <Select.Option value="2">
+//         Ms.Amala Rajakumara
+//       </Select.Option>
+//     </Select>
+//   </Form.Item>
+//   <Form.Item>
+//     <Button className="btn" type="primary" htmlType="submit">
+//       Create Report
+//     </Button>
+//   </Form.Item>
+// </Form>
+// </Card>
+// <Card className="reports">
+// <p className="T-report"> Analysis Report</p>
+// <Tabs defaultActiveKey="1" centered>
+//   <TabPane tab="Attendance Analysis of the Teacher" key="1">
+//     <Card Class name="details">
+//       <p>No of working days for the Month: </p>
+//       <p>No of periods for the Month : </p>
+//       <p>No of students assign for the teacher : </p>
+//       <p>No of leaves applied for the Month : </p>
+//       <h3>
+//         Attendance Percentage of ..(entered techer name)...
+//         for the Month of ...(Entered month)...{" "}
+//       </h3>
+//       <Progress
+//         ClassName="chart"
+//         type="circle"
+//         percent={75}
+//         format={(percent) => `${percent}% `}
+//       />
+//     </Card>
+//   </TabPane>
+//   <TabPane tab="Attendance Analysis of Students" key="2">
+//     <p>No of Students enrolled in particular subject : </p>
+//     <p>Required attendance for the month : </p>
+//     <p>Marked attendance for the month : </p>
+//     <h3> Attendance Analysis for the ..Subject.. </h3>
+//     <Progress
+//       ClassName="chart"
+//       type="circle"
+//       percent={75}
+//       format={(percent) => `${percent}% `}
+//     />
+//   </TabPane>
+//   <TabPane tab="Subject Preference Status" key="3">
+//     <p>Total No of Students enrolled for ..Subject.. : </p>
+//     <p>No of students enrolled with teacher .. name.. : </p>
+//     <h3>Subject preference status</h3>
+//     <Progress
+//       ClassName="chart"
+//       type="circle"
+//       percent={75}
+//       format={(percent) => `${percent}% `}
+//     />
+//   </TabPane>
+// </Tabs>
+// </Card>
--- src/pages/teacher/Dashboard.jsx
+++ src/pages/teacher/Dashboard.jsx
@@ -6,14 +6,13 @@ import img1 from "../../img/td11.jpg";
 import img2 from "../../img/t4.jpg";
 import "../student/dashboard.scss";
 import { Link } from "react-router-dom";
+import TeacherTimleline from "components/TeacherTimleline";
 
 //import classroomservice from "services/classroom.service";
 //import axios from "axios";
 //import { authHeader } from "utils/authheader";
 //import { apiurl } from "utils/common";
 //import authenticationservice from "services/authentication.service";
-
-
 
 var adata = [
   {
@@ -49,10 +48,10 @@ const tdata = [
 ];
 
 const { Meta } = Card;
-const subjects = ["Maths", "Chemistry", "Physics", "English", "Biology" , "IT"];
+const subjects = ["Maths", "Chemistry", "Physics", "English", "Biology", "IT"];
 const gridStyle = {
-  width: '25%',
-  textAlign: 'center',
+  width: "25%",
+  textAlign: "center",
 };
 const cstyle = {
   padding: 10,
@@ -60,14 +59,14 @@ const cstyle = {
   marginBottom: 0,
   minHeight: 280,
 };
-  
 
 export default function Dashboard() {
   const [schoolSubs, setSchoolSubs] = useState([]);
   const [tutionClasses, setTutionClasses] = useState([]);
   const [classroonName, setClassroonName] = useState("");
 
-  {/*useEffect(() => {
+  {
+    /*useEffect(() => {
     let userid = authenticationservice.currentUserValue.id;
     classroomservice
       .getSubDetailsForStudentDash()
@@ -92,116 +91,82 @@ export default function Dashboard() {
         message.error(e.response.data.message);
       });
   }, []);*/
-}
+  }
 
   return (
     <ContentLayout title="DashBoard" paths={["Home", "Dashboard"]}>
-      
-      <Row gutter={[10, 0]}>  
-        <div className="teacher-header">
-              <img style={{ width: 1450, height:200 }} src={img1} alt="img1" />
-        </div>
-            
+      <Row gutter={[10, 0]}>
+        <Col xs={24}>
+          <img
+            style={{ width: "100%", height: 200, objectFit: "cover" }}
+            src={img1}
+            alt="img1"
+          />
+        </Col>
       </Row>
       <br />
       <Row gutter={[10, 0]}>
         <Col xs={24} xl={18}>
-            <Card title="Your Classes" className="classcard" >
-                  {subjects.map((subject, index) => {
-                      return (
-                        <Link>
-                        <Card.Grid
-                          key={index}
-                          title={subject}
-                          style={{ gridStyle, width: 330, margin: 5 }}
-                        >
-                          {subject}
-                        </Card.Grid>
-                        </Link>
-                      );
-                    })}
-            </Card>
+          <Card title="Your Classes" className="classcard">
+            {subjects.map((subject, index) => {
+              return (
+                <Link>
+                  <Card.Grid
+                    key={index}
+                    title={subject}
+                    style={{ gridStyle, width: 330, margin: 5 }}
+                  >
+                    {subject}
+                  </Card.Grid>
+                </Link>
+              );
+            })}
+          </Card>
 
-            <div className="site-card-border-less-wrapper-2">
-              <Card
-                title="Announcements"
-                className="anncard"
-                style={cstyle}
-              >
-                <List
-                  itemLayout="horizontal"
-                  dataSource={adata}
-                  renderItem={(item) => (
-                    <List.Item>
-                      <List.Item.Meta
-                        style={{ marginLeft: 10 }}
-                        // avatar={
-                        //   <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                        // }
-                        title={
-                          <a href="http://localhost:3000/dashboard">
-                            {item.title}
-                          </a>
-                        }
-                        description={item.dis}
-                      />
-                    </List.Item>
-                  )}
-                />
-              </Card>
-            </div>
+          <div className="site-card-border-less-wrapper-2">
+            <Card title="Announcements" className="anncard" style={cstyle}>
+              <List
+                itemLayout="horizontal"
+                dataSource={adata}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      style={{ marginLeft: 10 }}
+                      // avatar={
+                      //   <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                      // }
+                      title={
+                        <a href="http://localhost:3000/dashboard">
+                          {item.title}
+                        </a>
+                      }
+                      description={item.dis}
+                    />
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </div>
         </Col>
 
         <Col xs={24} xl={6}>
+          <Card
+            style={{ marginBottom: 10 }}
+            cover={<img alt="example" src={img2} />}
+          >
+            <Meta title="InCharge Class" description="Grade Class" />
+          </Card>
 
-            <Card
-                style={{  }}
-                cover={
-                  <img
-                    alt="example"
-                    src={img2}
-                  />
-                }
-              >
-                <Meta
-                  title="InCharge Class"
-                  description="Grade Class"
-                />
-            </Card>,
-         
-            <Card
-              title="Today's Timeline"
-              className="timelinecard"
-              style={{ marginBottom: 10 }}
-            >
-              <Timeline>
-                <Timeline.Item>
-                  <Button type="link">Thermal physics lesson 7</Button> 2021-09-01
-                </Timeline.Item>
-                <Timeline.Item>
-                  <Button type="link">Thermal physics quiz 2</Button> 2021-09-06
-                </Timeline.Item>
-                <Timeline.Item
-                  dot={<ClockCircleOutlined className="timeline-clock-icon" />}
-                  color="red"
-                >
-                  <Button type="link">Electronic Lesson 1</Button> 2021-09-10
-                </Timeline.Item>
-                <Timeline.Item>
-                  <Button type="link">Electronic quiz 1</Button> 2015-09-01
-                </Timeline.Item>
-              </Timeline>
-            </Card> 
+          <TeacherTimleline />
         </Col>
-
       </Row>
       {/* </Content> */}
     </ContentLayout>
   );
 }
 
-
-{/*<Card title="Your Classes">
+{
+  /*<Card title="Your Classes">
             <div className="card-wrapper-student-dash">
               {tutionClasses.map((item, i) => (
                 <Link key={i} to={"/tution/subject/" + item.id}>
@@ -211,4 +176,5 @@ export default function Dashboard() {
                 </Link>
               ))}
             </div>
-          </Card>*/}
+          </Card>*/
+}
